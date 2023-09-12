88e1be920c2ca515a64e2e548a579e0b052da6d9
Rajesh Kanna A <128392370+rajeshk18@users.noreply.github.com>
2023-09-12 19:08:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import streamlit as st
+from streamlit_option_menu import option_menu
+import easyocr
+import mysql.connector as sql
+from PIL import Image
+import cv2
+import os
+import matplotlib.pyplot as plt
+import re
 
+# SETTING PAGE CONFIGURATIONS
+icon = Image.open("icon.png")
+st.set_page_config(page_title= "BizCardX: Extracting Business Card Data with OCR | By Jafar Hussain",
+                   page_icon= icon,
+                   layout= "wide",
+                   initial_sidebar_state= "expanded",
+                   menu_items={'About': """# This OCR app is created by *Jafar Hussain*!"""})
+st.markdown("<h1 style='text-align: center; color: white;'>BizCardX: Extracting Business Card Data with OCR</h1>", unsafe_allow_html=True)
+
+# SETTING-UP BACKGROUND IMAGE
+def setting_bg():
+    st.markdown(f""" <style>.stApp {{
+                        background: url("https://cutewallpaper.org/22/plane-colour-background-wallpapers/189265759.jpg");
+                        background-size: cover}}
+                     </style>""",unsafe_allow_html=True) 
+setting_bg()
